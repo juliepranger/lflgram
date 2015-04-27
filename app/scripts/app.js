c7eb6c8498bda66f7ec4359a55073e2cb3474b0d
Julie Bouck <julie@Julies-iMac.local>
2015-04-27 10:46:16 -0700

    added lfl logo, title, captions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,15 @@
 // Instantiates global instances / events
 (function($) {
 
+	//private global
+	var clientId_ = '64448854b9e84ee9b0986e5faf529c7a';
+
     var feed = new Instafeed({
         get: 'tagged',
-        clientId: '64448854b9e84ee9b0986e5faf529c7a', // hide this
+        clientId: clientId_, // hide this var better
         tagName: 'leftfieldlabs',
         resolution: 'low_resolution',
-    	template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+    	template: '<div class="image-container"><a href="{{link}}" target="_blank"><img src="{{image}}" /><p class="image-caption">{{caption}}</p></a></div>'
     });
     feed.run();
 
