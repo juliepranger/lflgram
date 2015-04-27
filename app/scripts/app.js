33d837b9121842ec57d3010881bcc213b2a3b9a4
Julie Bouck <julie@Julies-iMac.local>
2015-04-27 15:05:31 -0700

    added load more button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,17 +6,30 @@
 
 	//private global
 	var clientId_ = '64448854b9e84ee9b0986e5faf529c7a';
+	var loadMore = document.getElementById('load-more');
 
     var feed = new Instafeed({
         get: 'tagged',
         clientId: clientId_, // hide this var better
         tagName: 'leftfieldlabs',
         resolution: 'low_resolution',
+        limit: 200,
     	template: '<div class="image-container"><a href="{{link}}" target="_blank"><img src="{{image}}" />' +
     	'<p class="image-caption">{{caption}}</p>' +
     	'</a></div>'
     });
 
     feed.run();
+
+    loadMore.addEventListener('click', function() {
+    	var result = feed.next();
+    	feed.next();
+
+    	if (result == false) {
+    		loadMore.classList.add('hidden');
+    	}
+    });
+
+
 
 })(jQuery);
